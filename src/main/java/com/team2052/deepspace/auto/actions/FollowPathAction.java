217ffb5d47393170ea8468358b3b38b59ae41636
IanOlds <Istina668!>
2019-01-12 15:48:48 -0600

    copied autokrawler into deepspace

---
+++ src/main/java/com/team2052/deepspace/auto/actions/FollowPathAction.java
@@ -0,0 +1,52 @@
+package com.team2052.deepspace.auto.actions;
+
+import com.team2052.deepspace.auto.PurePursuitPathFollower;
+import com.team2052.lib.Autonomous.Path;
+
+public class FollowPathAction implements Action{
+
+    private PurePursuitPathFollower pathFollower = PurePursuitPathFollower.getInstance();
+
+    private Path path;
+    private Direction direction;
+
+    public FollowPathAction(Path path, Direction direction){
+        this.path = path;
+        this.direction = direction;
+    }
+
+    @Override
+    public void done() {
+        pathFollower.stopPathFollower();
+        System.out.println("Done With Path");
+
+    }
+
+    @Override
+    public boolean isFinished() {
+        return pathFollower.isPathComplete();
+    }
+
+    @Override
+    public void start() {
+        System.out.println("starting auto");
+        pathFollower.start(path, direction.isForward);
+    }
+
+    @Override
+    public void update() {
+        pathFollower.update();
+    }
+
+    public enum Direction{
+        FORWARD(true),
+        BACKWARD(false)
+        ;
+
+        public final boolean isForward;
+
+        Direction(boolean isForward){
+            this.isForward = isForward;
+        }
+    }
+}
---
+++ src/main/java/com/team2052/deepspace/auto/modes/Test.java
@@ -0,0 +1,38 @@
+package com.team2052.deepspace.auto.modes;
+
+import com.team2052.deepspace.auto.AutoMode;
+import com.team2052.deepspace.auto.actions.FollowPathAction;
+import com.team2052.lib.Autonomous.Path;
+import com.team2052.lib.Autonomous.Position2d;
+import com.team2052.lib.Autonomous.Waypoint;
+
+public class Test extends AutoMode{
+
+
+    @Override
+    protected void init() {
+        Path testPath = new Path();
+        Path testBPath = new Path();
+
+        System.out.println("init");
+        testPath.addWaypoint(new Waypoint(new Position2d(0,0), 60));
+        testPath.addWaypoint(new Waypoint(new Position2d(90,0), 60));
+        testPath.addWaypoint(new Waypoint(new Position2d(94,-2), 60));
+        testPath.addWaypoint(new Waypoint(new Position2d(96,-6), 60));
+        testPath.addWaypoint(new Waypoint(new Position2d(96,-54), 60));
+        testPath.addWaypoint(new Waypoint(new Position2d(98,-58), 60));
+        testPath.addWaypoint(new Waypoint(new Position2d(102,-60), 60));
+        testPath.addWaypoint(new Waypoint(new Position2d(138,-58), 60));
+        testPath.addWaypoint(new Waypoint(new Position2d(142,-56), 60));
+        testPath.addWaypoint(new Waypoint(new Position2d(144,-52), 60));
+        testPath.addWaypoint(new Waypoint(new Position2d(144,-6), 60));
+        testPath.addWaypoint(new Waypoint(new Position2d(146,-2), 60));
+        testPath.addWaypoint(new Waypoint(new Position2d(150,0), 60));
+        testPath.addWaypoint(new Waypoint(new Position2d(200,0), 60));
+        runAction(new FollowPathAction(testPath, FollowPathAction.Direction.FORWARD));
+
+        testBPath.addWaypoint(new Waypoint(new Position2d(48,0), 40));
+        testBPath.addWaypoint(new Waypoint(new Position2d(0,0), 40));
+        //runAction(new FollowPathAction(testBPath, FollowPathAction.Direction.BACKWARD));
+    }
+}
