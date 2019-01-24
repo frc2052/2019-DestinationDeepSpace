fa42fb08df5716e57256f2a1b68de13e6ed2355b
Sana <quruxland@gmail.com>
2019-01-23 19:47:11 -0600

    Added single pathes back in Started working on Auto Selector And added Auto Mode Defanitions for all single pathes as well.

---
+++ src/main/java/com/team2052/deepspace/auto/modes/RightStart/RightStartSideRightMiddleHatch.java
@@ -0,0 +1,24 @@
+package com.team2052.deepspace.auto.modes.RightStart;
+
+import com.team2052.deepspace.auto.AutoMode;
+import com.team2052.deepspace.auto.actions.FollowPathAction;
+import com.team2052.deepspace.auto.actions.HatchAction;
+import com.team2052.deepspace.auto.actions.SeriesAction;
+import com.team2052.deepspace.auto.paths.Path;
+import com.team2052.deepspace.auto.paths.RightStart.RStartSideRightMiddleHatchPath;
+
+import java.util.Arrays;
+
+public class RightStartSideRightMiddleHatch extends AutoMode {
+    @Override
+    protected void init() {
+        Path firstPath = new RStartSideRightMiddleHatchPath();
+        //Path secondPath = new RMiddleHatchStartRightHatchPickUpPath();
+        runAction(new SeriesAction(Arrays.asList(
+                new FollowPathAction(firstPath),
+                new HatchAction(HatchAction.Mode.OUT),
+                // new FollowPathAction(secondPath),
+                new HatchAction(HatchAction.Mode.IN)
+        )));
+    }
+}
