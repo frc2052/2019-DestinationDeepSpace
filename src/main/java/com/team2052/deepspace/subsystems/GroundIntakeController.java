44f95df88358c9d9b5b28aeb41826da78c49af67
lloydhilsgen001 <2020hilsl93@moundsviewschools.org>
2019-01-26 10:54:38 -0600

    Added Joysticks, and Outtake

---
+++ src/main/java/com/team2052/deepspace/subsystems/GroundIntakeController.java
@@ -0,0 +1,110 @@
+package com.team2052.deepspace.subsystems;
+
+import com.team2052.deepspace.Constants;
+import com.team2052.deepspace.KnightTimer;
+import com.team2052.deepspace.Loopable;
+import edu.wpi.first.wpilibj.Solenoid;
+
+public class GroundIntakeController implements Loopable {
+    /* True is Intake, false is Outtake.
+    Could be enum, but because intake and outtake already are enums with a done case
+    There's no reason complicate it. */
+    public void groundIntake(boolean state) {
+        if(state){
+            groundState = true;
+            intakeState = groundIntakeState.GRABBING;
+        } else {
+            groundState = false;
+            outtakeState = groundOuttakeState.RELEASING;
+        }
+        timer.start();
+    }
+
+    private boolean groundState;
+    private KnightTimer timer = new KnightTimer();
+    private groundOuttakeState outtakeState;
+    private groundIntakeState intakeState; //Non-Static Context
+
+    private static GroundIntakeController instance = new GroundIntakeController();
+    public static GroundIntakeController getInstance(){
+        return instance;
+    }//Singleton
+
+    public final Solenoid Lifter = new Solenoid(Constants.Controls.kLifterId);
+    public final Solenoid Grabber = new Solenoid(Constants.Controls.kGrabberId);
+
+    private boolean liftGroundIntakeState; //Getter and Setter
+    public boolean getLiftGroundIntakeState(){ return liftGroundIntakeState; }
+    public void setLiftGroundIntakeState(boolean state){
+        liftGroundIntakeState = state;
+        Lifter.set(state);
+    }
+    private boolean grabGroundIntakeState; //Getter and Setter
+    public boolean getGrabGroundIntakeState(){ return grabGroundIntakeState; }
+    public void setGrabGroundIntakeState(boolean state){
+        grabGroundIntakeState = state;
+        Grabber.set(state);
+    }
+
+    private void setIntakeState (groundIntakeState state){
+        intakeState = state;
+    } //only this class should want to change where it is in the enum
+    public boolean groundIntakeState(){
+        return ((intakeState == groundIntakeState.DONE));
+    } //Simplified Getter, since nothing should want to know if it's anything but done/not done
+    public boolean groundOuttakeState(){
+        return ((outtakeState == groundOuttakeState.DONE));
+    } //Simplified Getter, since nothing should want to know if it's anything but done/not done
+
+    public void update(){
+        if(groundState) {
+            switch (intakeState) {
+                case GRABBING:
+                    setGrabGroundIntakeState(true);
+                    if (timer.hasPassedTime(Constants.Intake.kGrabTime)) {
+                        setIntakeState(groundIntakeState.LIFTING);
+                    }
+                case LIFTING:
+                    setLiftGroundIntakeState(true);
+                    if (timer.hasPassedTime(Constants.Intake.kLiftTime)) {
+                        setIntakeState(groundIntakeState.DONE);
+                    }
+                case DONE:
+                    timer.stop();
+                    timer.reset();//doesn't matter if you keep telling it to stop/reset as long as you start it without these applying
+            }
+        } else {
+            switch(outtakeState){
+                case RELEASING:
+                    setGrabGroundIntakeState(false);
+                    if(timer.hasPassedTime(Constants.Intake.kReleaseTime)) {
+                        setIntakeState(groundIntakeState.LIFTING);
+                    }
+                case LOWERING:
+                    setLiftGroundIntakeState(false);
+                    if(timer.hasPassedTime(Constants.Intake.kEscapeTime)) {
+                        setIntakeState(groundIntakeState.DONE);
+                    }
+                case DONE:
+                    timer.stop();
+                    timer.reset();//doesn't matter if you keep telling it to stop/reset as long as you start it without these applying
+            }
+        }
+    } //Continuously updates, when state is set to grabbing, it goes through the cases over Constants.Intake.kLiftTime, then restarts the timer.
+
+    public void onStart(){ }//Requirement of Loopable
+    public void onStop(){ }//Requirement of Loopable
+
+
+    private enum groundIntakeState {
+        GRABBING,
+        LIFTING,
+        DONE
+    }
+    private enum groundOuttakeState {
+        RELEASING,
+        LOWERING,
+        DONE
+    } //Probably not needed, but it's better xplicit what is happening.
+
+}
---
+++ src/main/java/com/team2052/deepspace/subsystems/IntakeController.java
@@ -0,0 +1,48 @@
+package com.team2052.deepspace.subsystems;
+
+import com.ctre.phoenix.motorcontrol.ControlMode;
+import com.ctre.phoenix.motorcontrol.can.TalonSRX;
+import com.team2052.deepspace.Constants;
+import edu.wpi.first.wpilibj.Solenoid;
+
+public class IntakeController {
+    private static IntakeController instance = new IntakeController();
+    public static IntakeController getInstance() {return instance;}
+
+    private TalonSRX intakeMotor = new TalonSRX (Constants.Controls.kIntakeMotorId);
+    private Solenoid cargoIntakeSolenoid = new Solenoid(Constants.Controls.kCargoId);
+    private Solenoid hatchIntakeSolenoid = new Solenoid(Constants.Controls.kHatchId);
+
+    public void setCargoIntake(double speed) {
+        intakeMotor.set(ControlMode.PercentOutput, speed);
+    } //Sets cargo intake motor (In percent) to the speed you send it.
+
+    private boolean cargoIntakeState;
+    public boolean getCargoIntakeState() {
+        return cargoIntakeState;
+    }
+    public void setCargoIntakeState(boolean state){
+        cargoIntakeState = state;
+        cargoIntakeSolenoid.set(cargoIntakeState);
+    } //Getter and setter for cargo state
+
+    public void cargoNeutral(){
+        setCargoIntake(Constants.Intake.kNeutralSpeed);
+    }
+    public void cargoIntake(){
+        setCargoIntake(Constants.Intake.kIntakePercentSpeed);
+    }
+    public void cargoOuttake(){
+        setCargoIntake(Constants.Intake.kOuttakePercentSpeed);
+    } //Three speeds for the wheel.
+
+    private boolean hatchIntakeState;
+    public boolean getHatchIntakeState() {
+        return hatchIntakeState;
+    }
+    public void setHatchIntakeState(boolean state) {
+        hatchIntakeState = state;
+        hatchIntakeSolenoid.set(hatchIntakeState);
+    } //Getter and Setter for the HatchState
+
+}
