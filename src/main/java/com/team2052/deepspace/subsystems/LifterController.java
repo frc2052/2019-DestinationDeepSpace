73fb7e692afa442b4779c19d7882deeacf03e9d5
wafflewhal3 <quruxland@gmail.com>
2019-04-20 12:01:10 -0500

    uncommented limit switch in climber, added timer to lifter

--- src/main/java/com/team2052/deepspace/subsystems/LifterController.java
+++ src/main/java/com/team2052/deepspace/subsystems/LifterController.java
@@ -2,6 +2,7 @@ package com.team2052.deepspace.subsystems;
 
 import com.team2052.deepspace.Constants;
 import edu.wpi.first.wpilibj.Solenoid;
+import edu.wpi.first.wpilibj.Timer;
 
 public class LifterController {
     private static LifterController instance = null;
@@ -18,10 +19,24 @@ public class LifterController {
     }
 
     private Solenoid liftOutSolenoid = new Solenoid(Constants.Lifter.kLifterOutSolenoidId);
+    private int lifterButtonPressCount = 0;
+    private boolean wasLastPressed = false;
 
     public void setLegsDown(boolean isPressed) {
-        System.out.println("Lifter setting to " + isPressed);
-        liftOutSolenoid.set(isPressed);
+
+        double timeLeft = Timer.getMatchTime(); //this SHOULD be the time remaining in the match
+
+        if (!wasLastPressed){ //button state has changed, was up and is now down
+            lifterButtonPressCount++; //keep track of how many times the button was pressed
+        }
+
+        //keep track of whether button is up or down
+        wasLastPressed = true;
+
+        if ((timeLeft < 30 && lifterButtonPressCount > 0) || lifterButtonPressCount > 10) {
+            System.out.println("Lifter setting to " + isPressed);
+            liftOutSolenoid.set(isPressed);
+        }
     }
 
 }
