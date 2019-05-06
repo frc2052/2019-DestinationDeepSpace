7c6b902719a145b13d3c5cba62476a7851fbf282
IanOlds <ian.olds@team2052.com>
2019-05-06 17:29:13 -0500

    added path to the side for 2 hatch

---
+++ src/main/java/com/team2052/deepspace/auto/paths/HatchPickUp/RHatchPickUpStartRightCloseTwoHatchPathCompoundPath.java
@@ -0,0 +1,54 @@
+package com.team2052.deepspace.auto.paths.HatchPickUp;
+
+import com.team2052.deepspace.Constants;
+import com.team2052.deepspace.auto.paths.CompoundPath;
+import com.team2052.deepspace.auto.paths.Path;
+import com.team2052.lib.Autonomous.Position2d;
+import com.team2052.lib.Autonomous.Waypoint;
+
+import java.util.List;
+
+public class RHatchPickUpStartRightCloseTwoHatchPathCompoundPath extends CompoundPath {
+
+    public RHatchPickUpStartRightCloseTwoHatchPathCompoundPath() {
+        addPath(new  RHatchPickUpStartRightCloseHatchPath1());
+        addPath(new  RHatchPickUpStartRightCloseHatchPath2());
+    }
+
+    class   RHatchPickUpStartRightCloseHatchPath1 extends Path {
+        public  RHatchPickUpStartRightCloseHatchPath1() {
+            setDirection(Direction.BACKWARD);
+            isHighGear = true;
+            addWaypoint(new Waypoint(new Position2d(-30,125), Constants.Autonomous.kHighGearAutoVelocity));
+            addWaypoint(new Waypoint(new Position2d(60,90),Constants.Autonomous.kHighGearAutoVelocity));
+            addWaypoint(new Waypoint(new Position2d(195,70),Constants.Autonomous.kHighGearAutoVelocity));
+            addWaypoint(new Waypoint(new Position2d(195,80  ),Constants.Autonomous.kHighGearAutoVelocity));
+            forceQuickOptimization();
+        }
+
+        @Override
+        public List<Waypoint> getWaypoints() {
+            return wayPoints;
+        }
+
+    }
+
+    class  RHatchPickUpStartRightCloseHatchPath2 extends Path {
+        public  RHatchPickUpStartRightCloseHatchPath2() {
+            setDirection(Direction.FORWARD);
+            isHighGear = true;
+
+            addWaypoint(new Waypoint(new Position2d(195,80),Constants.Autonomous.kHighGearAutoVelocity));
+            addWaypoint(new Waypoint(new Position2d(195,65),Constants.Autonomous.kHighGearAutoVelocity));
+            forceQuickOptimization();
+        }
+
+        @Override
+        public List<Waypoint> getWaypoints() {
+            return wayPoints;
+        }
+
+    }
+
+}
+
